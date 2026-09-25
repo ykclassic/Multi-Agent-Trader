f9e80779daf6b6dffedca7fb7dca842fb790b4dc
TechSolute <pharshiesammy@gmail.com>
2026-09-25 22:00:50 +0100

    feat(foundation): add lib/supabase/client.ts

---
+++ lib/supabase/client.ts
@@ -0,0 +1,2 @@
+import {createBrowserClient} from "@supabase/ssr";
+export function createSupabaseBrowserClient(){return createBrowserClient(process.env.NEXT_PUBLIC_SUPABASE_URL!,process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!)}
